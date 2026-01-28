ba2646809b22352226119b24624880f40026751d
Hülya Başaran <142313783+Hulika8@users.noreply.github.com>
2026-01-28 17:10:42 +0300

    Add API throttling and update permissions docs (#6)

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -139,6 +139,16 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/hour',
+        'user': '1000/hour',
+        'register': '10/hour',
+        'login': '20/hour',
+    },
 }
 
 SIMPLE_JWT = {
